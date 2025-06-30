bd146e60cd42f2387027a0fa154723b227a40223
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2025-06-30 17:38:09 +0300

    feat: update application structure with routing and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Customers from "./pages/Customers/Customers";
+import Orders from "./pages/Orders/Orders";
+import { useAuth } from "./contexts/AuthContext";
 import './App.css'
 
 function App() {
 
+const { user } = useAuth();
 
   return (
-    <>
-      <h1>Welcome to the React App</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={user ? "/dashboard" : "/login"} />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            user ? <Dashboard /> : <Navigate to="/login" replace />
+          }
+        />
+        <Route
+          path="/customers"
+          element={user ? <Customers /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/orders"
+          element={user ? <Orders /> : <Navigate to="/login" replace />}
+        />
+        {/* TODO: add more protected routes like /customers, /orders */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import { useAuth } from "../../contexts/AuthContext";
 import { Button } from "antd";
 import { dashboardContainer } from "./styles";
+import MainLayout from "../../layouts/MainLayout";
+
 
 const Dashboard = () => {
   const { user, logout } = useAuth();
@@ -10,11 +12,13 @@ const Dashboard = () => {
     navigate("/login");
   }
   return (
+    <MainLayout>
     <div style={dashboardContainer}>
       <h1>Welcome, {user?.name}!</h1>
       <p>Your role: <strong>{user?.role}</strong></p>
       <Button type="primary" danger onClick={handleLogout}>Logout</Button>
     </div>
+    </MainLayout>
   );
 };
 
