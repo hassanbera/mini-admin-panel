a1efae805ceeba2af51a42c59a58285357d212ac
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2025-07-01 14:14:19 +0300

    feat: refactor App component to use RouteManager for improved routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Customers from "./pages/Customers/Customers";
-import Orders from "./pages/Orders/Orders";
-import { useAuth } from "./contexts/AuthContext";
-import './App.css'
-
+import './App.css';
+import { AuthProvider } from './contexts/AuthContext';
+import RouteManager from './routes/RouteManager';
 function App() {
-
-const { user } = useAuth();
-
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to={user ? "/dashboard" : "/login"} />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            user ? <Dashboard /> : <Navigate to="/login" replace />
-          }
-        />
-        <Route
-          path="/customers"
-          element={user ? <Customers /> : <Navigate to="/login" replace />}
-        />
-        <Route
-          path="/orders"
-          element={user ? <Orders /> : <Navigate to="/login" replace />}
-        />
-        {/* TODO: add more protected routes like /customers, /orders */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <RouteManager/>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
